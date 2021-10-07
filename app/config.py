76f32497a758d497c121becf8088dc5bf7a98194
alexander <a.barmin@napoleonit.ru>
2021-10-07 22:32:39 +0300

    ref: reformat code

--- app/config.py
+++ app/config.py
@@ -15,8 +15,8 @@ class Settings(BaseSettings):
     database_url: AnyUrl = os.environ.get("DATABASE_URL")
 
     class Config:
-        env_file = '.env'
-        env_file_encoding = 'utf-8'
+        env_file = ".env"
+        env_file_encoding = "utf-8"
 
 
 @lru_cache()
